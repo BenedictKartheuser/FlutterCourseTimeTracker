1f18f3e99042b35cec27659d2a3284f41026a531
Benedict Kartheuser <kartbene@gmx.de>
2020-04-10 01:46:04 +0200

    added functionality to create and edit jobs from app created edit/add page to do this used Form added validation and error handling

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:timetrackerfluttercourse/app/home/jobs_page.dart';
+import 'package:timetrackerfluttercourse/app/home/jobs/jobs_page.dart';
 import 'package:timetrackerfluttercourse/app/sign_in/sign_in_page.dart';
 import 'package:timetrackerfluttercourse/services/auth.dart';
 import 'package:timetrackerfluttercourse/services/database.dart';
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -4,22 +4,24 @@ import 'package:timetrackerfluttercourse/services/api_path.dart';
 import 'package:timetrackerfluttercourse/services/firestore_service.dart';
 
 abstract class Database {
-  Future<void> createJob(Job job);
+  Future<void> setJob(Job job);
   Stream<List<Job>> jobsStream();
 }
+
+String documentIdFromCurrentDate() => DateTime.now().toIso8601String();
 
 class FirestoreDatabase implements Database {
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
   final String uid;
   final _service = FirestoreService.instance;
 
-  Future<void> createJob(Job job) async => await _service.setData(
-        path: ApiPath.job(uid, 'job_abc'),
+  Future<void> setJob(Job job) async => await _service.setData(
+        path: ApiPath.job(uid, job.id),
         data: job.toMap(),
       );
 
   Stream<List<Job>> jobsStream() => _service.collectionStream(
         path: ApiPath.jobs(uid),
-        builder: (data) => Job.fromMap(data),
+        builder: (data, documentId) => Job.fromMap(data, documentId),
       );
 }
