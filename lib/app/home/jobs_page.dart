1ab5d17eb861e6080399b35079a98b937138c37e
Benedict Kartheuser <kartbene@gmx.de>
2020-04-09 18:42:02 +0200

    showing list of Jobs in Hobs page refactored Firebase access with Firebase Service class

--- lib/app/home/jobs_page.dart
+++ lib/app/home/jobs_page.dart
@@ -1,8 +1,13 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:timetrackerfluttercourse/app/home/models/job.dart';
 import 'package:timetrackerfluttercourse/common_widgets/platform_alert_dialog.dart';
+import 'package:timetrackerfluttercourse/common_widgets/platform_exception_alert_dialog.dart';
 import 'package:timetrackerfluttercourse/services/auth.dart';
+import 'package:timetrackerfluttercourse/services/database.dart';
 import 'package:timetrackerfluttercourse/services/database.dart';
 
 class JobsPage extends StatelessWidget {
@@ -12,6 +17,18 @@ class JobsPage extends StatelessWidget {
       await auth.signOut();
     } catch (e) {
       print(e.toString());
+    }
+  }
+
+  Future<void> _createJob(BuildContext context) async {
+    try {
+      final database = Provider.of<Database>(context);
+      await database.createJob(Job(name: 'Blogging', ratePerHour: 10));
+    } on PlatformException catch (e) {
+      PlatformExceptionAlertDialog(
+        title: 'Operation failed',
+        exception: e,
+      ).show(context);
     }
   }
 
@@ -25,11 +42,6 @@ class JobsPage extends StatelessWidget {
     if (didRequestSignOut == true) {
       _signOut(context);
     }
-  }
-
-  Future<void> _createJob(BuildContext context) async {
-    final database = Provider.of<Database>(context);
-    await database.createJob(Job(name: 'Blogging', ratePerHour: 10));
   }
 
   @override
@@ -50,10 +62,29 @@ class JobsPage extends StatelessWidget {
           ),
         ],
       ),
+      body: _buildContents(context),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () => _createJob(context),
       ),
+    );
+  }
+
+  Widget _buildContents(BuildContext context) {
+    final database = Provider.of<Database>(context);
+    return StreamBuilder<List<Job>>(
+      stream: database.jobsStream(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final jobs = snapshot.data;
+          final children = jobs.map((job) => Text(job.name)).toList();
+          return ListView(children: children);
+        }
+        if (snapshot.hasError) {
+          return Center(child: Text('Some error occured'));
+        }
+        return Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,20 +1,25 @@
-
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:timetrackerfluttercourse/app/home/models/job.dart';
 import 'package:timetrackerfluttercourse/services/api_path.dart';
+import 'package:timetrackerfluttercourse/services/firestore_service.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
+  Stream<List<Job>> jobsStream();
 }
 
 class FirestoreDatabase implements Database {
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
   final String uid;
+  final _service = FirestoreService.instance;
 
-  Future<void> createJob(Job job) async {
-    final path = ApiPath.job(uid, 'job_abc');
-    final documentReference = Firestore.instance.document(path);
-    await documentReference.setData(job.toMap());
-  }
+  Future<void> createJob(Job job) async => await _service.setData(
+        path: ApiPath.job(uid, 'job_abc'),
+        data: job.toMap(),
+      );
+
+  Stream<List<Job>> jobsStream() => _service.collectionStream(
+        path: ApiPath.jobs(uid),
+        builder: (data) => Job.fromMap(data),
+      );
 }
